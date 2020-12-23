1cf443c291e05e5c59b3318fb7b6296167f37655
Victor Radael <victor.radael@psvsistemas.com.br>
2020-12-23 19:52:26 -0300

    Deploy Test

--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 
 import api from '../../services/api';
 
-import { userTestToken } from '../../credentials';
+import userTestToken from '../../credentials';
 
 import {
   Container,
